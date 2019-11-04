86ace3483d7c8b07050e1f3a73f82bbbc0280ad2
Reto Weiss <reto.weiss@ivyteam.ch>
2019-11-04 23:38:35 +0100

    Requires modules from jdk with public API to load classes from it

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,7 @@ module ch.rweiss.whatisnew.java
 {
   requires org.apache.commons.lang3;
   requires java.se;
-  requires java.logging;
+  requires java.logging;   
+  requires jdk.jshell;
+  requires jdk.net;
 }
